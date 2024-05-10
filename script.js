62b0eaab6dc6cfba1d70620c1b1612d8d34ffd4b
Fernando Graciano <fcgraciano@hotmail.com>
2024-05-10 13:45:12 -0300

    acerto da pesquisa

--- script.js
+++ script.js
@@ -1,18 +1,89 @@
 
-function carregamento()
-{
-    let carregado = true;
-    //alert(carregado);
-    let url = "https://viacep.com.br/ws/17011067/json/";
+var grupo = 1 // 1 é o Id do professor
+var Id_Participante = localStorage.getItem("Id_Participante")
+function IniciarPesquisa() {
+
+    let url = "https://localhost:7045/api/Participante";
     var conexao = new XMLHttpRequest();
-    conexao.open("get",url);
-    conexao.send();
-    conexao.onload = function()
-    {
-        //alert(JSON.parse(conexao.response).logradouro);
+    conexao.open("post", url);
+    conexao.setRequestHeader("Content-type", "application/json");
+    conexao.send(JSON.parse(document.querySelector("#Idade").value));
+    conexao.onload = function () {
+        let retorno = JSON.parse(conexao.response)
+        console.log(retorno)
+        console.log(conexao.response)
+        localStorage.setItem("Id_Participante", retorno.id_participante)
+        window.location.href = "passo1.html";
     }
 }
-function trocarPasso(passoAtual, proximoPasso)
-{
-    window.location.href = proximoPasso;
+
+function carregamento(passo) {
+    let Id_Participante = localStorage.getItem("Id_Participante");
+    let url = "https://localhost:7045/api/Respostas";
+    let dados = `{ 
+            "id_participante": "${Id_Participante}",
+            "IsSaida": "false",
+            "passo": "${passo}",
+            "grupoId": "${grupo}"
+    }`
+    var conexao = new XMLHttpRequest();
+    conexao.open("post", url);
+    conexao.setRequestHeader("Content-type", "application/json");
+    console.log(dados)
+    conexao.send(dados)
+    conexao.onload = function () {
+        console.log("Carregado")
+        console.log(conexao.response)
+    }
+
+}
+
+
+function trocarPasso(passoAtual, proximoPasso) {
+
+    let url = "https://localhost:7045/api/Respostas";
+    let dados = `{ 
+        "id_participante": "${Id_Participante}",
+        "IsSaida": "false",
+        "passo": "${passoAtual}",
+        "grupoId": "${grupo}"
+}`
+    var conexao = new XMLHttpRequest();
+    conexao.open("post", url);
+    conexao.setRequestHeader("Content-type", "application/json");
+    conexao.send(dados)
+    conexao.onload = function () {
+        console.log("Carregado")
+        console.log(conexao.response)
+        let resposta = JSON.parse(conexao.response)
+        console.log(resposta)
+        console.log(resposta.liberado)
+        if (resposta.liberado) {
+            window.location.href = proximoPasso;
+        } else {
+            alert("erro verfique o console")
+        }
+    }
+
+}
+
+function Finalizar(){
+    
+    let url = "https://localhost:7045/api/Participante/Finalizar";
+    var conexao = new XMLHttpRequest();
+    conexao.open("post", url);
+    conexao.setRequestHeader("Content-type", "application/json");
+    conexao.send(
+        `{
+            "comentario" : "${document.querySelector("#interface").value + document.querySelector("#melhoria").value }", 
+            "id" : "${Id_Participante}" 
+        }`)
+    conexao.onload = function () {
+        let resposta = JSON.parse(conexao.response)
+        console.log(resposta)
+        if(resposta.sucesso){
+            alert("Obrigado por participar da pesquisa")
+        }
+        
+    }
 }
